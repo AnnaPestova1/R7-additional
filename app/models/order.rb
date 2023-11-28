1bbdab606e9365604da9c1183b2bcbecf5f78c7b
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-27 20:52:35 -0500

    1-8

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  validates_associated :customer
+
+
+ validates :product_name, presence: true
+ validates :product_count, numericality: true
+ validates :customer_id, presence: true
+
+ 
+
+
 end
+
