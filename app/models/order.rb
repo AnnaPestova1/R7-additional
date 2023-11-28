f69d7336d90fc7a638cbe787328022ce6a3ec54c
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-11-28 13:43:39 -0500

    some changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
 
 
  validates :product_name, presence: true
+ validates :product_count, presence: true
  validates :product_count, numericality: { only_integer: true }
  validates :customer_id, presence: true
 
